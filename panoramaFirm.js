3b84f22ca6de1102be278d0cfd6a6624766edf7c
Tomek <64986254+tomaszeq75@users.noreply.github.com>
2020-09-08 08:27:10 +0200

    initial

---
+++ panoramaFirm.js
@@ -0,0 +1,641 @@
+var markers = [{
+    "coordinates": {
+        "lat": 51.5956,
+        "lon": 18.73029
+    },
+    "companies": [{
+        "name": "Mariusz Biczysko",
+        "slug": "\u0142\u00f3dzkie,sieradzki,sieradz,20\/mariusz_biczysko-zwyxdz_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22559570,
+        "tradeId": 8210,
+        "address": "98-200 Sieradz 20  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": null,
+            "phone": {
+                "area": null,
+                "number": "880040619",
+                "formatted": "880 040 619"
+            }
+        },
+        "companyHash": "zwyxdz",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.64916,
+        "lon": 19.38067
+    },
+    "companies": [{
+        "name": "Mariusz Kania",
+        "slug": "\u0142\u00f3dzkie,pabianicki,pabianice,smugowa,18_lok._1\/mariusz_kania-zmfaeo_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 19990560,
+        "tradeId": 8210,
+        "address": "ul. Smugowa 18 lok. 1, 95-200 Pabianice  \u0142\u00f3dzkie",
+        "contact": null,
+        "companyHash": "zmfaeo",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.07457,
+        "lon": 19.40572
+    },
+    "companies": [{
+        "name": "Mariusz Marciniak Firma Sta\u015b",
+        "slug": "\u0142\u00f3dzkie,radomszcza\u0144ski,radomsko,brze\u017anicka,118\/mariusz_marciniak_firma_stas-zsxbwc_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 21194380,
+        "tradeId": 8210,
+        "address": "ul. Brze\u017anicka 118, 97-500 Radomsko  \u0142\u00f3dzkie",
+        "contact": null,
+        "companyHash": "zsxbwc",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.37579,
+        "lon": 18.6149
+    },
+    "companies": [{
+        "name": "Mariusz Marczak Auto - Handel",
+        "slug": "\u0142\u00f3dzkie,wielu\u0144ski,ole\u015bnica,20\/mariusz_marczak_auto_handel-zhhkpx_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 18700197,
+        "tradeId": 8210,
+        "address": "98-311 Ole\u015bnica 20  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": null,
+            "phone": {
+                "area": null,
+                "number": "694482296",
+                "formatted": "694 482 296"
+            }
+        },
+        "companyHash": "zhhkpx",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.49314,
+        "lon": 19.00556
+    },
+    "companies": [{
+        "name": "Mariusz Marlicki Maniek-Car",
+        "slug": "\u0142\u00f3dzkie,\u0142aski,nowe_kozuby,12\/mariusz_marlicki_maniek_car-zkxpri_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 21863937,
+        "tradeId": 8210,
+        "address": "98-160 Nowe Kozuby 12  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "maniek4128@wp.pl",
+            "phone": {
+                "area": null,
+                "number": "663818277",
+                "formatted": "663 818 277"
+            }
+        },
+        "companyHash": "zkxpri",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.80867,
+        "lon": 19.36373
+    },
+    "companies": [{
+        "name": "Mariusz Ma\u015blak \u0022Emtrans\u0022",
+        "slug": "\u0142\u00f3dzkie,,\u0142\u00f3d\u017a,ba\u0142uty,hektarowa,41\/mariusz_maslak_emtrans-zksour_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 21773237,
+        "tradeId": 8210,
+        "address": "ul. Hektarowa 41, 91-230 \u0141\u00f3d\u017a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "maslakmariusz@wp.pl",
+            "phone": null
+        },
+        "companyHash": "zksour",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.7744,
+        "lon": 20.4293
+    },
+    "companies": [{
+        "name": "Marjo Pawe\u0142 Rochmi\u0144ski",
+        "slug": "\u0142\u00f3dzkie,rawski,p\u0105g\u00f3w,3\/marjo_pawel_rochminski-acxnpi_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 9587865,
+        "tradeId": 8210,
+        "address": "96-230 P\u0105g\u00f3w 3  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "pawroch@vp.pl",
+            "phone": {
+                "area": null,
+                "number": "609515631",
+                "formatted": "609 515 631"
+            }
+        },
+        "companyHash": "acxnpi",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.73556,
+        "lon": 19.44103
+    },
+    "companies": [{
+        "name": "\u0022Marmaris\u0022 PPUH Mariusz Matejas",
+        "slug": "\u0142\u00f3dzkie,,\u0142\u00f3d\u017a,g\u00f3rna,jasie\u0144,38_40\/marmaris_ppuh_mariusz_matejas-zmtshn_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 20136589,
+        "tradeId": 8210,
+        "address": "ul. Jasie\u0144 38\/40, 93-562 \u0141\u00f3d\u017a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": null,
+            "phone": {
+                "area": "42",
+                "number": "6849484",
+                "formatted": "(42) 684 94 84"
+            }
+        },
+        "companyHash": "zmtshn",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.77942,
+        "lon": 19.41074
+    },
+    "companies": [{
+        "name": "Mar-Pol-Serwis Marek Borucki",
+        "slug": "\u0142\u00f3dzkie,,\u0142\u00f3d\u017a,polesie,bobowa,18\/mar_pol_serwis_marek_borucki-zlewhe_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 19862420,
+        "tradeId": 8210,
+        "address": "ul. Bobowa 18, 94-248 \u0141\u00f3d\u017a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": null,
+            "phone": {
+                "area": null,
+                "number": "509783987",
+                "formatted": "509 783 987"
+            }
+        },
+        "companyHash": "zlewhe",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.95729,
+        "lon": 19.28019
+    },
+    "companies": [{
+        "name": "Martu\u015b-Trans Marta Fabrykowska",
+        "slug": "\u0142\u00f3dzkie,zgierski,ozork\u00f3w,tkacka,8_lok._15\/martus_trans_marta_fabrykowska-zxmltl_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 23065163,
+        "tradeId": 8210,
+        "address": "ul. Tkacka 8 lok. 15, 95-035 Ozork\u00f3w  \u0142\u00f3dzkie",
+        "contact": null,
+        "companyHash": "zxmltl",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.93195,
+        "lon": 19.65136
+    },
+    "companies": [{
+        "name": "Marwel Marek Walerych",
+        "slug": "\u0142\u00f3dzkie,zgierski,bratoszewice,szkolna,15a\/marwel_marek_walerych-zwlwzy_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22392071,
+        "tradeId": 8210,
+        "address": "ul. Szkolna 15a, 95-011 Bratoszewice  \u0142\u00f3dzkie",
+        "contact": null,
+        "companyHash": "zwlwzy",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.71935,
+        "lon": 19.43143
+    },
+    "companies": [{
+        "name": "Mask Marek Sk\u00f3rski",
+        "slug": "\u0142\u00f3dzkie,,\u0142\u00f3d\u017a,g\u00f3rna,pok\u0142adowa,24\/mask_marek_skorski-axpzmi_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 15124905,
+        "tradeId": 8210,
+        "address": "ul. Pok\u0142adowa 24, 93-492 \u0141\u00f3d\u017a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "mask.lodz@gmail.com",
+            "phone": null
+        },
+        "companyHash": "axpzmi",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.53829,
+        "lon": 20.03214
+    },
+    "companies": [{
+        "name": "Mason-Power Daniel Majewski",
+        "slug": "\u0142\u00f3dzkie,tomaszowski,tomasz\u00f3w_mazowiecki,spalska,64_lok._4\/mason_power_daniel_majewski-zxdein_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 23208421,
+        "tradeId": 8210,
+        "address": "ul. Spalska 64 lok. 4, 97-200 Tomasz\u00f3w Mazowiecki  \u0142\u00f3dzkie",
+        "contact": null,
+        "companyHash": "zxdein",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.22585,
+        "lon": 18.55663
+    },
+    "companies": [{
+        "name": "Mateusz Chadrjan",
+        "slug": "\u0142\u00f3dzkie,wielu\u0144ski,wielu\u0144,wyspia\u0144skiego,5\/mateusz_chadrjan-zthjua_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 21350449,
+        "tradeId": 8210,
+        "address": "ul. Wyspia\u0144skiego 5, 98-300 Wielu\u0144  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "mateuszchadrjan@gmail.com",
+            "phone": {
+                "area": null,
+                "number": "792240330",
+                "formatted": "792 240 330"
+            }
+        },
+        "companyHash": "zthjua",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.65396,
+        "lon": 18.56059
+    },
+    "companies": [{
+        "name": "Mateusz Kubiczak Mat - Auto",
+        "slug": "\u0142\u00f3dzkie,sieradzki,tub\u0105dzin,59\/mateusz_kubiczak_mat_auto-zblflj_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 17076370,
+        "tradeId": 8210,
+        "address": "98-285 Tub\u0105dzin 59  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "swietlik@autograf.pl",
+            "phone": {
+                "area": null,
+                "number": "691187955",
+                "formatted": "691 187 955"
+            }
+        },
+        "companyHash": "zblflj",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.50864,
+        "lon": 19.39305
+    },
+    "companies": [{
+        "name": "Mateusz Wadowski",
+        "slug": "\u0142\u00f3dzkie,be\u0142chatowski,zwierzyniec,45a\/mateusz_wadowski-zwngri_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22412865,
+        "tradeId": 8210,
+        "address": "97-403 Zwierzyniec 45A  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "matwad@wp.pl",
+            "phone": {
+                "area": null,
+                "number": "606680904",
+                "formatted": "606 680 904"
+            }
+        },
+        "companyHash": "zwngri",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.74576,
+        "lon": 19.47101
+    },
+    "companies": [{
+        "name": "Mateusz Z\u0105becki",
+        "slug": "\u0142\u00f3dzkie,,\u0142\u00f3d\u017a,g\u00f3rna,d\u0119bowa,4_lok._60\/mateusz_zabecki-zwkieu_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22469682,
+        "tradeId": 8210,
+        "address": "ul. D\u0119bowa 4 lok. 60, 93-102 \u0141\u00f3d\u017a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "mateusz.zabecki@interia.pl",
+            "phone": {
+                "area": null,
+                "number": "883900104",
+                "formatted": "883 900 104"
+            }
+        },
+        "companyHash": "zwkieu",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.17279,
+        "lon": 19.33765
+    },
+    "companies": [{
+        "name": "Mateusz \u017burawski",
+        "slug": "\u0142\u00f3dzkie,radomszcza\u0144ski,brudzice,\u0142\u00f3dzka,15a\/mateusz_zurawski-zxbeyo_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22946088,
+        "tradeId": 8210,
+        "address": "ul. \u0141\u00f3dzka 15A, 97-565 Brudzice  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": null,
+            "phone": {
+                "area": null,
+                "number": "516933727",
+                "formatted": "516 933 727"
+            }
+        },
+        "companyHash": "zxbeyo",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.6207,
+        "lon": 18.89868
+    },
+    "companies": [{
+        "name": "Mati Cars Nyks Mateusz",
+        "slug": "\u0142\u00f3dzkie,zdu\u0144skowolski,izabel\u00f3w,56\/mati_cars_nyks_mateusz-zwyxxp_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22559558,
+        "tradeId": 8210,
+        "address": "98-220 Izabel\u00f3w 56  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": null,
+            "phone": {
+                "area": null,
+                "number": "605485569",
+                "formatted": "605 485 569"
+            }
+        },
+        "companyHash": "zwyxxp",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.62171,
+        "lon": 18.94192
+    },
+    "companies": [{
+        "name": "Mat\u0026Mar Marzena Szewczyk",
+        "slug": "\u0142\u00f3dzkie,zdu\u0144skowolski,janiszewice,4\/mat\u0026mar_marzena_szewczyk-zblgog_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 17076679,
+        "tradeId": 8210,
+        "address": "98-220 Janiszewice 4  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "mateo.com@wp.pl",
+            "phone": {
+                "area": null,
+                "number": "608194318",
+                "formatted": "608 194 318"
+            }
+        },
+        "companyHash": "zblgog",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.94606,
+        "lon": 19.75251
+    },
+    "companies": [{
+        "name": "Matmar Mateusz Biernacki",
+        "slug": "\u0142\u00f3dzkie,zgierski,piaski_rudnickie,28a\/matmar_mateusz_biernacki-zclnod_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 17411926,
+        "tradeId": 8210,
+        "address": "95-015 Piaski Rudnickie 28a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": null,
+            "email": "matekb@gmail.com",
+            "phone": {
+                "area": null,
+                "number": "509338434",
+                "formatted": "509 338 434"
+            }
+        },
+        "companyHash": "zclnod",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.85402,
+        "lon": 19.43857
+    },
+    "companies": [{
+        "name": "Mat-Pol Emilia Promi\u0144ska",
+        "slug": "\u0142\u00f3dzkie,zgierski,zgierz,kochanowskiego,27\/mat_pol_emilia_prominska-zwbyru_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 22283850,
+        "tradeId": 8210,
+        "address": "ul. Kochanowskiego 27, 95-100 Zgierz  \u0142\u00f3dzkie",
+        "contact": null,
+        "companyHash": "zwbyru",
+        "tradeHash": "pfz"
+    }]
+}, {
+    "coordinates": {
+        "lat": 51.71706,
+        "lon": 19.44674
+    },
+    "companies": [{
+        "name": "\u0022Matsuoka Motor\u0022",
+        "slug": "\u0142\u00f3dzkie,,\u0142\u00f3d\u017a,g\u00f3rna,3_maja,1_3\/matsuoka_motor-ablwxa_pfz.html",
+        "payable": 0,
+        "templateId": null,
+        "companyId": 9121465,
+        "tradeId": 8210,
+        "address": "ul. 3 Maja 1 \/3, 93-408 \u0141\u00f3d\u017a  \u0142\u00f3dzkie",
+        "contact": {
+            "www": "http:\/\/www.mazdalodz.pl",
+            "email": "salon@mazdalodz.pl",
+            "phone": {
+                "area": "42",
+                "number": "6530353",
+                "formatted": "(42) 653 03 53"
+            }
+        },
+        "companyHash": "ablwxa",
+        "tradeHash": "pfz"
+    }]
+}];
+var locationResult = {
+    "coordinates": {
+        "lat": null,
+        "lon": null
+    },
+    "zip": null,
+    "city": {
+        "name": "",
+        "id": -1
+    },
+    "street": null,
+    "district": "",
+    "county": {
+        "id": -1,
+        "name": ""
+    },
+    "voivodeship": {
+        "name": "\u0142\u00f3dzkie",
+        "id": 578370
+    },
+    "level": "voivodeship",
+    "id": 578370,
+    "common": ""
+};
+var phrase = 'samochody używane';
+var page = '31';
+var where = 'łódzkie';
+
+
+// wkleić to poniżej w konsoli strony
+
+function prepareData() {
+    let companiesData = "";
+    for (const index in markers) {
+        let company = markers[index].companies[0];
+        let phone = "";
+        if (company.contact != null && (company.contact.email != null || company.contact.www != null)) {
+            let email = company.contact.email ? company.contact.email : "";
+            let www = company.contact.www ? company.contact.www : "";
+            if (company.contact.phone) {
+                phone = company.contact.phone.formatted;
+            }
+            companiesData += `${company.name};${email};${company.address};${www};${phone}\n`;
+        }
+    }
+    console.log(companiesData);
+    download(companiesData, 'dane.csv', 'text/csv');
+
+    // następna strona 
+    let nextButton = document.getElementsByClassName("addax-cs_hl_nextpage")[0];
+    setTimeout(function () {
+        if (nextButton) {
+            nextButton.click();
+        } else {
+            alert("To była ostatnia strona!");
+        }
+    }, 500);
+}
+
+function download(content, fileName = 'dane.csv', contentType = 'text/csv') {
+    var a = document.createElement("a");
+    var file = new Blob([content], {
+        type: contentType
+    });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+prepareData();
+
+
+
+/// numery telefonów sprawdzanie czy nie stacjonarne
+
+function checkNumbers(){
+
+    let prefix = [12, 13, 14, 15, 16, 17, 18, 22, 23, 24,
+        25, 29, 32, 33, 34, 41, 42, 43, 44, 46, 48, 52, 54, 55, 56, 58, 59,
+        61, 62, 63, 65, 67, 68, 71, 74, 75, 76, 77, 81, 82, 83, 84, 85, 86, 87, 89, 91, 94, 95, 80, 70
+    ];
+    
+    let numbers = document.querySelector(".numbers").innerText.split(",");
+    let numbersFiltered = [];
+    console.log(numbers, prefix);
+    
+    function removeTooShort(nums) {
+        let newNums = [];
+        nums.forEach(element => {
+            if (element.length == 9) {
+                newNums.push(element)
+            }
+        });
+        console.log(newNums);
+        return newNums;
+    }
+    
+    function checkNumbers(numbers) {
+        let newNums = [];
+        let badNums = [];
+        for (let i = 0; i < numbers.length; i++) {
+            const first2Digits = +numbers[i].slice(0, 2);
+            prefix.forEach(element => {
+                console.log(element, typeof element, first2Digits, typeof first2Digits, element == first2Digits);
+                if (element == +first2Digits) {
+                    numbers.splice(i--,1);
+                    console.log(numbers[i],first2Digits);
+                    return;
+                } 
+            });
+        }
+        console.log(newNums);
+        console.log(badNums);
+    }
+    
+    numbersFiltered = removeTooShort(numbers)
+    checkNumbers(numbersFiltered);
+    document.querySelector(".filtered").innerText = numbersFiltered;
+}
+
+checkNumbers()
